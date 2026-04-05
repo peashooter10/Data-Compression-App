256f647e109befd5c354384887bef7205d2cdd38
peashooter10 <emanuelbarbu2004@gmail.com>
2026-04-05 21:17:20 +0300

    Incercare 3

--- Arbore.h
+++ Arbore.h
@@ -4,20 +4,29 @@
 #include <vector>
 using namespace std;
 
+const int MAX = 512;
+
 class Arbore {
 protected:
     int valoare;
-    vector<int> nod;
-    vector<int> pozitie;
-    vector<int> predecesori;
+    int nod[MAX];
+    int pozitie[MAX];
+    int predecesori[MAX];
+    int nrNoduri;
+    int copilStanga[MAX]; // 1 if this node is left child (0-bit), 0 if right (1-bit), -1 if root
+
 public:
-    Arbore(int valoare, vector<int> nod, vector<int> pozitie, vector<int> predecesori);
+    Arbore(int valoare = 0);
     ~Arbore();
 
-    Arbore fuzionareArbori(Arbore &arbore1, Arbore &arbore2, int nodUrmator);//fuzionare 2 arbori
+    static Arbore fuzionareArbori(Arbore &arbore1, Arbore &arbore2, int nodUrmator);
 
-    void afisareArbore(Arbore &arbore);
+    void initFrunza(int simbol, int frecventa);
+    int getValoare() const;
+
+    void afisareArbore();
+    void afisareCoduri(Arbore& arboreFinal, vector<int> &listaArbori);
 
 };
 
-#endif //HUFFMAN_ARBORE_H
+#endif
--- main.cpp
+++ main.cpp
@@ -1,45 +1,101 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 #include <iomanip>
+#include "Arbore.h"
+
 using namespace std;
 
 const int DIMENSIUNE = 256;
 
-
-
 int main() {
+    int i,sumaFrecvente=0,dimensiuneFisier=0;
     string fisier = "exemplu.txt";
     ifstream file(fisier, ios::binary);
 
     if (!file.is_open()) {
-        cerr << "eroare, nu exista fisierul\n";
+        cerr << "Eroare la deschiderea fisierului!\n";
         return 1;
     }
 
+    // --- citire fisier ---
     vector<unsigned char> buffer(
         (istreambuf_iterator<char>(file)),
-        (istreambuf_iterator<char>()));
+        (istreambuf_iterator<char>())
+    );
     file.close();
 
+    // --- calcul frecvente ---
     int frecventa[DIMENSIUNE] = {0};
-    for (size_t i = 0; i < buffer.size(); ++i) {
+
+    for (unsigned char c : buffer) {
+        frecventa[c]++;
+    }
+
+    dimensiuneFisier=buffer.size();
+
+    //contorizez
+    for (i=0; i<buffer.size(); ++i) {
         frecventa[buffer[i]]++;
     }
 
-
-
-    int sumaFrecvente = 0;
-    for (int i = 0; i < DIMENSIUNE; ++i) {
+    //
+    for (i = 0; i < 4096; ++i) {
         if (frecventa[i] > 0) {
             cout << "0x" << hex << setw(2) << setfill('0') << i
                  << " : " << dec << frecventa[i] << "\n";
-            sumaFrecvente += frecventa[i];
+            sumaFrecvente=sumaFrecvente+frecventa[i];
         }
     }
 
-    if (sumaFrecvente == (int)buffer.size())
-        cout << "\ne bine, sunt " << sumaFrecvente << " bytes\n";
+    if (sumaFrecvente==buffer.size())
+        cout<<"e bine, sunt "<< sumaFrecvente << " de bytes\n";
+
+
+    // --- creare arbori initiali ---
+    vector<Arbore> arbori;
+
+    for (int i = 0; i < DIMENSIUNE; i++) {
+        if (frecventa[i] > 0) {
+            Arbore a;
+            a.initFrunza(i, frecventa[i]);
+            arbori.push_back(a);
+        }
+    }
+
+    // --- construire arbore Huffman ---
+    int nodUrmator = 256;
+
+    while (arbori.size() > 1) {
+        sort(arbori.begin(), arbori.end(),
+            [](const Arbore &a, const Arbore &b) {
+                return a.getValoare() < b.getValoare();
+            });
+
+        Arbore nou = Arbore::fuzionareArbori(
+            arbori[0], arbori[1], nodUrmator++
+        );
+
+        arbori.erase(arbori.begin());
+        arbori.erase(arbori.begin());
+
+        arbori.push_back(nou);
+    }
+
+    if (arbori.empty()) {
+        cout << "Fisier gol.\n";
+        return 0;
+    }
+
+    Arbore arboreFinal;
+    arboreFinal=arbori[0];
+
+    // ===============================
+    // 🌳 AFISARE ARBORE
+    // ===============================
+    cout << "\n=== Arbore (X si U) ===\n";
+    arboreFinal.afisareArbore();
 
     return 0;
 }
